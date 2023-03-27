a87e539d67ff4336f9998fa7007cb2bc708395d8
Streptoco <81880440+Streptoco@users.noreply.github.com>
2023-03-27 20:50:25 +0300

    Succeeded in finding algorithm

--- Ex01_01/Main.cs
+++ Ex01_01/Main.cs
@@ -7,7 +7,6 @@ using System.Threading.Tasks;
 
 public class Program
 {
-
     // TODO: 1. check for kelet lo huki 2. 
     public static void Main()
     {
@@ -17,10 +16,38 @@ public class Program
             string readNumberFromUser = Console.ReadLine();
             while (readNumberFromUser.Length != 8  && !isValid(readNumberFromUser))
             {
+                Console.WriteLine("Please enter a VALID number!");
                 readNumberFromUser = Console.ReadLine();
             }
-            int currentNumberParsing = Convert.ToInt32(readNumberFromUser, 2);
-            Console.WriteLine(currentNumberParsing);
+            short numberConvertedToDecimalFromBinary = ConvertToDecimal(readNumberFromUser);
+            Console.WriteLine(numberConvertedToDecimalFromBinary);
         }
     }
+
+    public static bool isValid(string readNumberFromUser)
+    {
+        for(int i = 0; i < 8; i++)
+        {
+            if (readNumberFromUser[i] != 1 || readNumberFromUser[i] != 0)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static short ConvertToDecimal(string readNumberFromUser)
+    {
+        short convertedNumber = 0;
+        for(int i = 0;  i <= 7; i++)
+        {
+            short power = (short)(readNumberFromUser[i] - '0');
+            if(power != 0)
+            {
+                convertedNumber += (short)Math.Pow(2, power * (7 - i));
+            }
+            Console.WriteLine(convertedNumber);
+        }
+        return convertedNumber;
+    } // 11100110 => 230
 }
