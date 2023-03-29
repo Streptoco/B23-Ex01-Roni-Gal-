46ef49789967d6c5eb0188b3549e85a386922963
Streptoco <81880440+Streptoco@users.noreply.github.com>
2023-03-29 20:00:30 +0300

    Ex2 done

--- Ex01_02/Main.cs
+++ Ex01_02/Main.cs
@@ -1,12 +1,53 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace Ex01_02
+public class Program
 {
-    internal class Main
+    public static void Main()
     {
+        printDiamond(1, 9);
+    }
+
+    public static void printRow(int numberOfStarsInRow, int numberOfSpacesInRow, int fixedRowLength)
+    {
+        if (fixedRowLength == 0)
+        {
+            Console.Write('\n');
+            return;
+        }
+        else if(numberOfSpacesInRow > 0) // Invoke the left space-bars in the pattern
+        {
+            Console.Write(" ");
+            printRow(numberOfStarsInRow, numberOfSpacesInRow-1, fixedRowLength-1);
+        }
+        else if(numberOfStarsInRow > 0 ) // Invoke the stars in the pattern
+        {
+            Console.Write("*");
+            printRow(numberOfStarsInRow - 1, numberOfSpacesInRow, fixedRowLength - 1);
+        }
+        else // Invoke the right space-bars in the pattern
+        {
+            Console.Write(" ");
+            printRow(numberOfStarsInRow, numberOfSpacesInRow, fixedRowLength - 1);
+        }
+    }
+
+    public static void printDiamondRecursive(int numberOfStars, int height)
+    {
+        if(height < numberOfStars)
+        {
+            return;
+        }
+        printRow(numberOfStars, (height - numberOfStars)/2, height);
+        printDiamondRecursive(numberOfStars + 2, height);
+        if(height == numberOfStars)
+        {
+            return;
+        }
+        printRow(numberOfStars, (height - numberOfStars)/2, height);
+    }
+
+    public static void printDiamond(int numberOfStars, int height)
+    {
+        printDiamondRecursive(numberOfStars, height);
     }
 }
