8c5fae127d9d50350926e8568cab1c721d7e8090
Streptoco <81880440+Streptoco@users.noreply.github.com>
2023-03-30 15:02:35 +0300

    finished ex04

--- Ex01_04/Main.cs
+++ Ex01_04/Main.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Text;
 
+public enum eChoice { isNumberAndDividesBy3 = 0, isNumberAndDoesntDivideBy3, IsNotANumber }
+
 public class Program
 {
+    
     public static void Main()
     {
         Console.WriteLine("Please enter a string containing 6 chars");
@@ -16,19 +19,34 @@ public class Program
         // Method to check if it's a palindrome
         Console.WriteLine(isPalindrome(inputFromUser) ? "The string is a palindrome." : "The string isn't a palindrome.");
         // Method to check if the number divides by 3
+        eChoice doesTheNumberDivideByThree = doesItDivideByThree(inputFromUser);
+        if(doesTheNumberDivideByThree == eChoice.IsNotANumber)
+        {
+            Console.WriteLine("The string isn't a number.");
+        }
+        else if(doesTheNumberDivideByThree == eChoice.isNumberAndDoesntDivideBy3)
+        {
+            Console.WriteLine("The string is a number, but it's not divisible by three.");
+        }
+        else
+        {
+            Console.WriteLine("The string is a number, and it is divided by three.");
+        }
         // Method to check how many uppercase letters are there
+        int countUpperCaseLetters = countUppercaseLettersInString(inputFromUser);
+        Console.WriteLine($"The number of uppercase letters in the string is {countUpperCaseLetters}");
     }
     public static bool invalidInputChecker(string inputFromUser)
     {
-        if(inputFromUser.Length > 6)
+        if(inputFromUser.Length != 6)
         {
             return false;
         }
-        else if (inputFromUser[0] >= 'a' && inputFromUser[0] <= 'z' || inputFromUser[0] >= 'A' && inputFromUser[0] <= 'Z')
+        else if ((inputFromUser[0] >= 'a' && inputFromUser[0] <= 'z') || (inputFromUser[0] >= 'A' && inputFromUser[0] <= 'Z'))
         {
             for(int i = 1; i < inputFromUser.Length; i++)
             {
-                if((inputFromUser[i] < 'a' || inputFromUser[i] > 'z') || (inputFromUser[i] < 'A' && inputFromUser[i] > 'Z'))
+                if((inputFromUser[i] < 'a' || inputFromUser[i] > 'z') && (inputFromUser[i] < 'A' || inputFromUser[i] > 'Z'))
                 {
                     return false;
                 }
@@ -53,7 +71,7 @@ public class Program
     }
     public static bool isPalindrome(string inputFromUser)
     {
-        if(inputFromUser.Length == 1)
+        if(inputFromUser.Length == 1 || inputFromUser.Length == 0)
             return true;
         else if (inputFromUser[0] == inputFromUser[inputFromUser.Length-1])
         {
@@ -64,5 +82,35 @@ public class Program
         {
             return false;
         }
+    }
+
+    public static eChoice doesItDivideByThree(string inputFromUser)
+    {
+        int result;
+        if (int.TryParse(inputFromUser, out result))
+        {
+            if (result % 3 == 0)
+            {
+                return eChoice.isNumberAndDividesBy3;
+            }
+            else
+            {
+                return eChoice.isNumberAndDoesntDivideBy3;
+            }
+        }
+        return eChoice.IsNotANumber;
+    }
+
+    public static int countUppercaseLettersInString(string inputFromUser)
+    {
+        int countNumberOfUpperLetters = 0;
+        for(int i = 0; i < inputFromUser.Length; i++)
+        {
+            if(char.IsUpper(inputFromUser[i]))
+            {
+                countNumberOfUpperLetters++;
+            }
+        }
+        return countNumberOfUpperLetters;
     }
 }
